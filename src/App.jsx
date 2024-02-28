2bec4bb67beaf6dcc4e63f72d437e0c5a7da68e2
Mdsakib999 <sakibabrar999@gmail.com>
2024-02-28 14:28:57 +0600

    added total amount

--- src/App.jsx
+++ src/App.jsx
@@ -15,59 +15,109 @@ import { RxCrossCircled } from "react-icons/rx";
 import { LuGrab } from "react-icons/lu";
 import { GiTakeMyMoney } from "react-icons/gi";
 import { FaMoneyCheckDollar } from "react-icons/fa6";
+import { useState } from "react";
 
 
 function App() {
+
+  const [selectedProducts, setSelectedProducts] = useState([]);
+
   const products = [
     {
       id: 1,
       name: "Product 1",
       price: 19.99,
+      quantity: 1,
       image: "https://media.istockphoto.com/id/521705890/photo/little-boy-in-white-shirt.jpg?s=2048x2048&w=is&k=20&c=LwIXtdqyQkabODzRzvcjaJ8JbICo4RCVLcxLRPveFNY=",
     },
     {
       id: 2,
       name: "Product 2",
       price: 29.99,
+      quantity: 1,
       image: "https://media.istockphoto.com/id/467383049/photo/happy-little-boy-on-white-background.jpg?s=2048x2048&w=is&k=20&c=JJg1Od67NLaCJVfBCAY5aoluu8CBPZq51PeQ5NliCzE=",
     },
     {
       id: 3,
       name: "Product 3",
       price: 39.99,
+      quantity: 1,
       image: "https://img.freepik.com/premium-photo/closeup-red-blank-t-shirt-with-empty-space-yours-design-white-background-3d-rendering_476612-5218.jpg?w=740",
     },
     {
       id: 4,
       name: "Product 2",
       price: 29.99,
+      quantity: 1,
       image: "https://img.freepik.com/free-photo/young-boy-with-curly-hairs-wearing-eyeglasses_114579-17346.jpg?t=st=1709042348~exp=1709045948~hmac=2495600f3cdc32ca12a94a9cd88fc9958a40c87c6c65c9578045c4cfc33ca0af&w=360",
     },
     {
       id: 5,
       name: "Product 3",
       price: 39.99,
+      quantity: 1,
       image: "https://img.freepik.com/free-photo/young-man-making-call-me-gesture_329181-2147.jpg?w=360&t=st=1709042388~exp=1709042988~hmac=0bc370d5a3537a93fd9d37dc5842079370ea5bfa7861a73e442ad07bf3abd4b0",
     },
     {
       id: 6,
       name: "Product 6",
       price: 39.99,
+      quantity: 1,
       image: "https://img.freepik.com/free-photo/front-back-isolated-black-t-shirts_125540-1092.jpg?w=996&t=st=1709042531~exp=1709043131~hmac=73a804cd16e853a88c30adede7cd6da79f95b00152e9ae9a33a990d86e95dc86",
     },
     {
       id: 7,
       name: "Product 7",
       price: 29.99,
+      quantity: 1,
       image: "https://img.freepik.com/free-photo/young-boy-with-curly-hairs-wearing-eyeglasses_114579-17346.jpg?t=st=1709042348~exp=1709045948~hmac=2495600f3cdc32ca12a94a9cd88fc9958a40c87c6c65c9578045c4cfc33ca0af&w=360",
     },
     {
       id: 8,
       name: "Product 8",
       price: 39.99,
+      quantity: 1,
       image: "https://img.freepik.com/free-photo/young-man-making-call-me-gesture_329181-2147.jpg?w=360&t=st=1709042388~exp=1709042988~hmac=0bc370d5a3537a93fd9d37dc5842079370ea5bfa7861a73e442ad07bf3abd4b0",
     },
   ];
+
+  const addToSelectedProducts = (product) => {
+    setSelectedProducts([...selectedProducts, product]);
+    
+  };
+
+  const handleRemoveProduct = (productId) => {
+    setSelectedProducts(prevProducts => prevProducts.filter(product => product.id !== productId));
+  };
+
+  const handleAddQuantity = (id) => {
+    setSelectedProducts(prevProducts => {
+      return prevProducts.map(product => {
+        if (product.id === id) {
+          // Increment quantity by 1
+          return { ...product, quantity: product.quantity + 1 };
+        }
+        return product;
+      });
+    });
+  };
+
+  const handleRemoveQuantity = (id) => {
+    setSelectedProducts(prevProducts => {
+      return prevProducts.map(product => {
+        if (product.id === id && product.quantity > 1) {
+          // Decrement quantity by 1 if greater than 1
+          return { ...product, quantity: product.quantity - 1 };
+        }
+        return product;
+      });
+    });
+  };
+
+  const subTotal = selectedProducts.reduce(
+    (total, product) => total + product.price * product.quantity,
+    0
+  );
 
   return (
     <div className="flex ml-2 ">
@@ -105,6 +155,7 @@ function App() {
         </div>
         <MdOutlineDeleteForever className=" text-3xl ml-6 text-red-600"></MdOutlineDeleteForever>
       </div>
+
       <div className=" flex items-center">
       <SlNote className="text-3xl "></SlNote>
         <div className="flex items-center  border-r-2 border-l-2 border-b-2 ml-3 px-2 py-2 text-gray-500 text-xl">
@@ -117,6 +168,25 @@ function App() {
         </div>
         <MdOutlineDeleteForever className=" text-3xl ml-6 text-red-600"></MdOutlineDeleteForever>
       </div>
+
+      
+      {selectedProducts.map((product) => (
+          <div key={product.id} className=" flex items-center">
+            {/* Display product details */}
+            <SlNote className="text-3xl "></SlNote>
+            <div className="flex items-center border-r-2 border-l-2 border-b-2 ml-3 px-2 py-2 text-gray-500 text-xl">
+            <p className="mr-56">{product.name}</p>
+            <p className="mr-10">${product.price}</p>
+            <FaMinusCircle onClick={() => handleRemoveQuantity(product.id)} className="mr-8 cursor-pointer" />
+            {/* product quantity */}
+            <p className="mr-2">{product.quantity}</p>
+            <IoIosAddCircle onClick={() => handleAddQuantity(product.id)} className="mr-8 ml-7 text-2xl cursor-pointer" />
+            <p className="">${(product.quantity * product.price).toFixed(2)}</p>
+            </div>
+            <MdOutlineDeleteForever className="text-red-600 cursor-pointer ml-6 text-3xl" onClick={() => handleRemoveProduct(product.id)} />
+          </div>
+        ))}
+      
       
 
       {/* tax shipping discount */}
@@ -127,11 +197,11 @@ function App() {
           <hr />
           <div className="flex justify-between my-2">
             <p>Sub Total</p>
-            <p className="font-bold">$1500</p>
+            <p className="font-bold">${(subTotal +181).toFixed(2)}</p>
           </div>
           <hr />
           <div className="flex justify-between my-2">
-            <p>Sub Total</p>
+            <p>TAX</p>
             <p className="font-bold">$25.00</p>
           </div>
           <hr />
@@ -152,9 +222,9 @@ function App() {
       
       {/* total money */}
       <div className=" flex items-center justify-between px-5 py-3 mt-4 rounded-md bg-indigo-50 text-indigo-500">
-        <p>Products Count</p>
+        <p>Products Count ( {selectedProducts.length} )</p>
         <p className="text-xl font-semibold">Total</p>
-        <p className="text-xl font-semibold">$100</p>
+        <p className="text-xl font-semibold">${(subTotal +206).toFixed(2)}</p>
       </div>
 
     {/* last button */}
@@ -164,6 +234,15 @@ function App() {
         <button className=" px-4 py-2 rounded-md font-semibold text-xl bg-indigo-50 text-indigo-500"><GiTakeMyMoney className="inline text-3xl mr-2"></GiTakeMyMoney> Discount</button>
         <button className=" px-4 py-2 rounded-md font-semibold text-xl bg-indigo-50 text-indigo-500"><FaMoneyCheckDollar className="inline text-3xl mr-2"></FaMoneyCheckDollar> Pay Now</button>
       </div>
+
+
+      {selectedProducts.length >= 2 ? (
+        <p style={{ display: "none" }}>Placeholder</p>
+      ) : (
+        <p className="text-red-500 font-bold text-center mt-8">
+          Click on a Product to Add
+        </p>
+      )}
 
       </div>
 
@@ -198,18 +277,16 @@ function App() {
 
         {/* Products */}
         <div className="mt-5 grid grid-cols-5 gap-x-3 gap-y-6 pb-5 ps-3 cursor-pointer">
-          {
-            products.map((product) => (
-            <div className="border-2 border-gray-300" key={product.id}>
+        {products.map((product) => (
+            <div key={product.id} className="border-2 border-gray-300" onClick={() => addToSelectedProducts(product)}>
               <div className="">
-              <img className="h-[180px] w-full" src={product.image} alt="picture" />
+                <img className="h-[180px] w-full" src={product.image} alt="picture" />
               </div>
-              <p className="text-center py-1 font-semibold text-gray-500">${product.price}</p> 
+              <p className="text-center py-1 font-semibold text-gray-500">${product.price}</p>
               <hr />
               <p className="text-center py-1 font-semibold text-gray-500">{product.name}</p>
             </div>
-            ) )
-          }
+          ))}
         </div>
       </div>
     </div>
